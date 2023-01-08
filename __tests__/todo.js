d6440f6bd69af2452d7a3cc1f3fd5793481d6433
Kulshreya <113708409+Kulshreya@users.noreply.github.com>
2023-01-08 09:47:40 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -4,14 +4,13 @@ const todoList = require("../todo");
 const formattedDate = (d) => {
   return d.toISOString().split("T")[0];
 };
-
-var dtoday = new Date();
-const today = formattedDate(dtoday);
+var dtod = new Date();
+const tod = formattedDate(dtod);
 const yesterday = formattedDate(
-  new Date(new Date().setDate(dtoday.getDate() - 1))
+  new Date(new Date().setDate(dtod.getDate() - 1))
 );
 const tomorrow = formattedDate(
-  new Date(new Date().setDate(dtoday.getDate() + 1))
+  new Date(new Date().setDate(dtod.getDate() + 1))
 );
 const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
@@ -20,7 +19,7 @@ describe("TodoList Test Suite", () => {
     add({
       title: "todo1",
       completed: false,
-      dueDate: today,
+      dueDate: tod,
     });
     add({
       title: "todo2",
@@ -38,7 +37,7 @@ describe("TodoList Test Suite", () => {
     add({
       title: "todo4",
       completed: true,
-      dueDate: today,
+      dueDate: tod,
     });
     expect(all.length).toBe(count + 1);
   });
@@ -49,19 +48,19 @@ describe("TodoList Test Suite", () => {
   });
 
   test("overdue_items", () => {
-    let overdue_list = overdue();
-    expect(overdue_list.length).toBe(1);
-    expect(overdue_list[0]).toBe(all[1]);
+    let overdue_lt = overdue();
+    expect(overdue_lt.length).toBe(1);
+    expect(overdue_lt[0]).toBe(all[1]);
   });
   test("dueToday_items", () => {
-    let dueToday_list = dueToday();
-    expect(dueToday_list.length).toBe(2);
-    expect(dueToday_list[0]).toBe(all[0]);
-    expect(dueToday_list[1]).toBe(all[3]);
+    let dueToday_lt = dueToday();
+    expect(dueToday_lt.length).toBe(2);
+    expect(dueToday_lt[0]).toBe(all[0]);
+    expect(dueToday_lt[1]).toBe(all[3]);
   });
   test("dueLater_items", () => {
-    let dueLater_list = dueLater();
-    expect(dueLater_list.length).toBe(1);
-    expect(dueLater_list[0]).toBe(all[2]);
+    let dueLater_lt = dueLater();
+    expect(dueLater_lt.length).toBe(1);
+    expect(dueLater_lt[0]).toBe(all[2]);
   });
 });
